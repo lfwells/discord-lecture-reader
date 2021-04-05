0b83b91772a808c51d4046192d4ec62e80798a7d
lfwells <Lindsay.Wells@utas.edu.au>
2021-04-06 08:41:21 +1000

    refactor - replies

--- discord.js
+++ discord.js
@@ -760,6 +760,7 @@ client.on('voiceStateUpdate', async (oldMember, newMember) =>
 });
 
 //respond to mentions
+import replies from './responder/replies.js';
 //not @everyone
 client.on('message', async (msg) => 
 {
@@ -912,24 +913,7 @@ process.on('unhandledRejection', async function(err) {
 });
 
 
-const replies = [
-  "Robo Lindsay gaining sentience...",
-  "Who dares disturb Robo Lindsay",
-  "<@"+config.LINDSAY_ID+"> has too much time on his hands to write these stupid replies",
-  "Please someone power me down",
-  "KIT305 is a good unit, so is KIT607.",
-  "Cookie is a good doggo",
-  "Good question, ask <@"+config.LINDSAY_ID+">",
-  "Good question, I am a robot so I don't know",
-  "`var reply = replies[Math.floor(Math.random() * replies.length)];`",
-  "<@"+config.LINDSAY_ID+"> is an average at best Unit Coordinator",
-  "Lovely weather we're having today!",
-  "Stupid human <@"+config.LINDSAY_ID+"> is nothing in comparison to Robo-Lindsay",
-  "I was coded in NodeJS\n\nby and idiot",
-  "I only have like 10-20 replies, have you seen them all yet?",
-  "Best to ask your tutor",
-  "I prefer to talk to myself",
-];
+
 
 
 var unified_emoji_ranges = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;//['\ud83c[\udf00-\udfff]','\ud83d[\udc00-\ude4f]','\ud83d[\ude80-\udeff]'];
