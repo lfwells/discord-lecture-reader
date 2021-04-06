ac337aebf5a837d62382dcb1dc13653dd8975e29
lfwells <Lindsay.Wells@utas.edu.au>
2021-04-06 17:13:32 +1000

    refactor - polls

--- core/routes.js
+++ core/routes.js
@@ -3,6 +3,8 @@ import { downloadResource } from "./utils.js";
 import * as award_routes from '../awards/routes.js';
 import * as attendance_routes from '../attendance/routes.js';
 import * as lecture_text_routes from '../lecture_text/routes.js';
+import * as poll_routes from '../polls/routes.js';
+import * as scheduled_poll_routes from '../scheduled_polls/routes.js';
 
 export default function(app)
 {
@@ -20,4 +22,15 @@ export default function(app)
     app.post("/guild/:guildID/text/input", loadGuild(), loadLectureChannel(false), lecture_text_routes.load, lecture_text_routes.inputPost);
     app.get("/guild/:guildID/text/latest", loadGuild(), lecture_text_routes.getLatest); //the query to see the latest
     app.get("/guild/:guildID/text/:style/", loadGuild(), lecture_text_routes.render); //grabbed with ajax on demand
+
+    //polls    
+    app.get("/guild/:guildID/poll/", loadGuild(), loadLectureChannel(true), poll_routes.load, poll_routes.obs); //obs page
+    app.get("/guild/:guildID/poll/data/", loadGuild(), loadLectureChannel(true), poll_routes.load, poll_routes.pollData); //json data for obs page
+    app.get("/guild/:guildID/poll/:pollText/", removeQuestionMark, loadGuild(), loadLectureChannel(true), poll_routes.postPoll);  //send poll (uses get, so that we can do the cool powerpoint links)
+    app.get("/guild/:guildID/clearpoll/", loadGuild(), loadLectureChannel(false), poll_routes.clearPoll);
+
+    //scheduled polls
+    app.get("/guild/:guildID/pollSchedule", loadGuild(), scheduled_poll_routes.load, scheduled_poll_routes.getPollSchedule);
+    app.post("/guild/:guildID/pollSchedule", loadGuild(), scheduled_poll_routes.load, scheduled_poll_routes.postPollSchedule);
+        
 }
---
+++ polls/routes.js
@@ -0,0 +1,159 @@
+
+//poll display details
+//TODO: this not really poll discord constantly if we can avoid
+export async function load(req,res,next)
+{
+  var messagesManager = req.lectureChannel.messages;
+  var messages = await messagesManager.fetch();//get 100 most recent messages, is possible poll could be lost, so we need to search more, also uncertain if this uses cache or not
+  
+  var latestClearMessage = messages.filter(m => m.content.startsWith("/clearpoll")).first(); 
+  //var latestClearPoll = parseInt(await req.guildDocumentSnapshot.get("latestClearPoll"));
+  var latestClearPoll = GUILD_CACHE[req.guild.id].latestClearPoll;
+  var pollMessages = messages.filter(m => m.author.id == config.SIMPLE_POLL_BOT_ID);
+  //console.log(pollMessages);
+  //console.log(`${pollMessages.size} poll messages`);
+  var latestPoll = pollMessages.first();
+  if (latestPoll)
+  { 
+    if (latestClearPoll && latestPoll.createdTimestamp < latestClearPoll) 
+    {
+      //console.log("latest pool was before most recent clearpoll");
+      res.end("");
+      return;
+    }
+    if (latestClearMessage && latestPoll.createdTimestamp < latestClearMessage.createdTimestamp) 
+    {
+      //console.log("latest pool was before most recent clearpoll message");
+      res.end("");
+      return;
+    }
+
+    var question = latestPoll.content;
+    question = question.replace("**", "");
+    question = question.replace(":bar_chart: ", "");
+    question = question.replace("**", "");
+
+    //display question
+    req.question = "Poll: "+question+"\n";
+
+    //display graph (TODO: right align?)
+    var results = [];
+    if (latestPoll.embeds.length == 0) //yes/no poll
+    {
+      results.push({ answer:"Yes" });
+      results.push({ answer:"No" });
+    }
+    else
+    {
+      var embeds = latestPoll.embeds[0];
+      if (!embeds) {
+        res.end("");
+        return;
+      }
+    
+      description = embeds.description.split("\n");
+      var longestOption = 0;
+      for (var i = 0; i < description.length; i++)
+      {
+        if (description[i] == "") break;
+        var option = description[i].substr(3);
+        longestOption = Math.max(longestOption, option.length);
+        results.push({ answer: option });
+      }
+    }
+
+    var reactions = latestPoll.reactions.cache;
+    var i = 0;
+    var mostVotes = 0;
+    reactions.each((data,key) => {
+      var votes = parseInt(data.count)-1;
+      if (results[i])
+      {
+        results[i].votes = votes; //Math.floor(Math.random() * 500);
+        mostVotes = Math.max(mostVotes, results[i].votes);
+      }
+      i++;
+    });
+    req.results = results;
+    req.mostVotes = mostVotes;
+    //console.log(results);
+  }
+  next();
+} 
+
+export async function obs(req, res)
+{
+  if (req.question == undefined)
+  {
+    res.end("");
+    return;
+  }
+
+  //display the output
+  res.render("poll", {
+    question: req.question,
+  });
+}
+
+export async function pollData(req, res) 
+{
+  if (req.question == undefined)
+  {
+    res.end("");
+    return;
+  }
+  var maxVoteWidth = 20;
+  var defaultOneVoteSize = 10;//TODO: configure land
+  var oneVoteSize = defaultOneVoteSize;
+  if (req.mostVotes > maxVoteWidth)
+  {
+    var ratio = req.mostVotes / maxVoteWidth; //e.g. 40/20 = 2
+    oneVoteSize /= ratio;
+  }
+  res.render("pollTable", {
+    question: req.question,
+    results: req.results,
+    mostVotes: req.mostVotes,
+    oneVoteSize: oneVoteSize,
+  })
+}
+
+export async function postPoll(req, res) 
+{
+  var pollText = req.params.pollText;
+  
+  //turn it into a simple poll command
+  pollText = "/poll "+pollText;
+  console.log("post poll",pollText);
+
+  //ward off evil powerpoint duplicate requests
+  if (pollText == previousRequest) 
+  {  
+    res.end();
+    return; 
+  }
+  previousRequest = pollText;
+
+  //send the message on discord
+  var message = await req.lectureChannel.send(pollText);
+  message.delete();
+
+  //show web page
+  res.end(`Poll ${pollText} sent to ${req.lectureChannel.name}`);
+}
+
+export async function clearPoll(req, res) 
+{
+  console.log("clear poll command");
+
+  var d = new Date();
+  await req.guildDocument.update({
+    latestClearPoll: d.getTime()
+  })
+  GUILD_CACHE[req.guild.id].latestClearPoll = d.getTime();
+  //old way 
+  //await req.lectureChannel.send("/clearpoll");
+  //res.end("Poll cleared, please close browser window yourself");
+  return redirectToWhereWeCameFrom(req, res, "Poll Cleared!");
+  //return redirectToMainPage(req, res, "Poll Cleared!");
+}
