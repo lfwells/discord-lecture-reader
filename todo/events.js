77514b8829ea8c16ce127cf54d895e0f372c43d2
Lindsay Wells <lindsay.wells@utas.edu.au>
2021-11-04 06:40:05 +1100

    deferred replies for todo command

--- todo/events.js
+++ todo/events.js
@@ -7,6 +7,10 @@ export default async function(client)
         name: 'Mark TODO',
         type: "MESSAGE"
     }; 
+    const todoSlashCommand = {
+        name: 'todo',
+        description: "Mark a message as a TODO (sent to either TODO channel, or as a DM, depending on config)"
+    }; 
     
     var guilds = client.guilds.cache;
     await guilds.each( async (guild) => { 
@@ -16,6 +20,7 @@ export default async function(client)
             console.log(guild.name+"delete "+await command.delete());
         }
         /*console.log(guild.name+"add "+*/await guild.commands.create(todoCommand);//); 
+        /*console.log(guild.name+"add "+*/await guild.commands.create(todoSlashCommand);//); 
     });
 
     client.on('interactionCreate', async function(interaction) 
@@ -28,6 +33,10 @@ export default async function(client)
         {
             doTodoCommand(interaction);            
         }
+        if (interaction.commandName === "todo") 
+        {
+            doTodoCommand(interaction);             
+        }
     });
 
 }
@@ -36,7 +45,8 @@ async function doTodoCommand(interaction)
 {
     console.log("Todo command");
     //this can take too long to reply, so we immediately reply
-    //await interaction.deferReply();
+    await interaction.deferReply({ ephemeral: true });
+    //TODO: allow option to inform user that they have been marked as a todo?
 
     var originalMessage = await interaction.channel.messages.fetch(interaction.targetId);
     console.log(originalMessage);
@@ -52,17 +62,22 @@ async function doTodoCommand(interaction)
     //only allow if user is admin
     if (interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR) == false)
     {
-        interaction.user.send({ embeds:[message], content:"New TODO:" });
-        return;
-    }
-
-    var todoChannel = await getGuildPropertyConverted("todoChannelID", interaction.guild);
-    if (todoChannel)
-    {
-        todoChannel.send({ embeds:[message], content:"New TODO:" });   
+        var post = await interaction.user.send({ embeds:[message], content:"New TODO: "+originalMessage.url });
+        interaction.editReply("TODO sent as a DM "+post.url);
     }
     else
     {
-        interaction.user.send({ embeds:[message], content:"New TODO (note: you can get these messages in a todo channel if you configure it):" });
+
+        var todoChannel = await getGuildPropertyConverted("todoChannelID", interaction.guild);
+        if (todoChannel)
+        {
+            var post = await todoChannel.send({ embeds:[message], content:"New TODO:"+originalMessage.url });   
+            interaction.editReply("TODO sent to <#"+todoChannel.id+"> "+post.url);
+        }
+        else
+        {
+            var post = await interaction.user.send({ embeds:[message], content:"New TODO (note: you can get these messages in a todo channel if you configure it): "+originalMessage.url });
+            interaction.editReply("TODO send as a DM "+post.url);
+        }
     }
 }
