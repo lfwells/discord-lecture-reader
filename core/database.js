78c63e229e34b310eb176688b73c7b1a37fdcb55
lfwells <Lindsay.Wells@utas.edu.au>
2022-06-24 01:46:48 +1000

    oauth keys for discord

--- core/database.js
+++ core/database.js
@@ -1,7 +1,7 @@
 import admin from "firebase-admin"; 
 
 //var serviceAccount = require(path.join(__dirname, "partygolflite-firebase-adminsdk-dfc3p-8e78d63026.json"));
-import serviceAccount from '../carers-care-firebase-adminsdk-sp7cd-1a37ad2d83.js';
+import serviceAccount from '../google_cloud_key.js';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
