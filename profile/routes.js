62870e13647a93a3444a6b9ee61e812275fae56f
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-04-12 21:41:02 +1000

    added total posts to profile page

--- profile/routes.js
+++ profile/routes.js
@@ -1,5 +1,7 @@
 import { loadProfile } from "./profile.js";
 import { getClient } from '../core/client.js';
+import { getPostsFilterPredicate } from "../classList/classList.js";
+import { getPostsData } from "../analytics/analytics.js";
 
 export async function load(req, res, next)
 {
@@ -15,6 +17,13 @@ export async function load(req, res, next)
         {
             req.profile.guilds.push(guild);
         }
+    }
+
+    //read in the stats for each server
+    req.profile.total = 0;
+    for (let guild of req.profile.guilds)
+    {
+        req.profile.total += (await getPostsData(guild, null, (post) => post.author == req.profile.id)).length;
     }
 
     res.locals.profile = req.profile;
