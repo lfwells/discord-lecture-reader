0e85bc813980405fd63b74cc9ad2b459a3ae5724
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-04-12 21:06:21 +1000

    added profile picture to profile

--- profile/routes.js
+++ profile/routes.js
@@ -6,12 +6,22 @@ export async function load(req, res, next)
     req.profile = getClient().users.cache.get(req.params.discordID);
     
     req.profile = Object.assign(req.profile, (await loadProfile(req.params.discordID)).data());
+
+    //read in what guilds the user is in
+    req.profile.guilds = [];
+    for (let guild of getClient().guilds.cache.values())
+    {
+        if (guild.members.cache.has(req.profile.id))
+        {
+            req.profile.guilds.push(guild);
+        }
+    }
+
     res.locals.profile = req.profile;
     
     next();
 }
 export async function profile_home(req,res,next)
 {
-    var discordID = req.params.discordID;
     res.render("profile/index");
 }
